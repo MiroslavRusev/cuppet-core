78cc00e202a72f7a4a5fda0fe5dd73e654561ae3
miroslav.rusev <miroslav.rusev@living.homes>
2025-09-02 14:56:26 +0300

    Adjusting eslint config.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import html from '@html-eslint/eslint-plugin';
 import htmlParser from '@html-eslint/parser';
 
 export default defineConfig([
-    globalIgnores(['**yarn.lock', '**/node_modules/', '.git/']),
+    globalIgnores(['**yarn.lock', '**/node_modules/', '.git/', '**/backStopData/', '**/reports/']),
     {
         files: ['**/*.js'],
         plugins: {
